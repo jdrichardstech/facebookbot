561cde1199bf6a79cc7350aef788fe3bbf54e119
jdrichardstech <jdrichardstech@gmail.com>
2017-06-28 16:22:27 -0400

    refactor

--- index.js
+++ index.js
@@ -27,13 +27,42 @@ app.get('/webhook/', (req, res)=>{
 
 
 
-const token = 'EAAQNLEIhmisBANzygGhCleI2LhxfZAmTsOr3mrixDjFl3MC4ZBtZB2AH1z9s2v7dLCXjZCcdSTpg3mXc96VK5sUy2gZBRTfsArhy9qZBKGl7ciXNRfI4jzymJWfZAl4F8BYa5yRjIp9e014YtohjZBIFZAWJxx1doK3rF8m8Wn88MB3S3mudw8vgCUshUET9nk2YZD'
+const token = 'EAAQNLEIhmisBAAWsVLyaa0VrzS8x9LbjoEgZCCkAmyaxWCjwyEFiHGh2ysUP38TaMIofSUAx26cLP9rurVWRUuNxtyYQfO9LLSVzbg3jUo9kpLOt4nEo57GMwQ31ZARmjrvFcXlzccTS07fpmBDm8NduyXHpQNczxj3ZCT8vIsXFvs7l6aP7dhClWor1zkZD'
 
 
-function sendTextMessage(sender, text){
-	messageData = {
-		text:text
-	}
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: 'EAAQNLEIhmisBAAWsVLyaa0VrzS8x9LbjoEgZCCkAmyaxWCjwyEFiHGh2ysUP38TaMIofSUAx26cLP9rurVWRUuNxtyYQfO9LLSVzbg3jUo9kpLOt4nEo57GMwQ31ZARmjrvFcXlzccTS07fpmBDm8NduyXHpQNczxj3ZCT8vIsXFvs7l6aP7dhClWor1zkZD '},
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
 }
 
 	function sendGenericMessage(sender){
@@ -86,27 +115,67 @@ function sendTextMessage(sender, text){
 	})
 }
 
-app.post('/webhook/', function (req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)
-})
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
 
 app.listen(app.get('port'), ()=>{
 	console.log('running on port', app.get('port'))
