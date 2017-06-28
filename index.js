21e80e0ba72592663094281425beb2579e57427c
jdrichardstech <jdrichardstech@gmail.com>
2017-06-28 15:29:37 -0400

    add cards

--- index.js
+++ index.js
@@ -21,17 +21,7 @@ app.get('/webhook/', (req, res)=>{
 	res.send('Error, wrong token')
 })
 
-app.post('/webhook',(req, res)=>{
-	messaging_events = req.body.entry[0].messaging
-	for(i=0;i<messaging_events.length; i++){
-		event = req.body.entry[0].messaging[i]
-		sender = event.sender.id
-		if(event.message && event.message.text){
-			text=event.message.textsendTextMessage(sender, "Text received, echo: " + text.substring(0,200))
-		}
-	}
-	res.sendStatus(200)
-})
+
 
 const token = 'EAAQNLEIhmisBAA0WCNUP7gqBEQZBcEmDrTZBL6XcxlnY33282dAWpANOsatgZCTCIhHqX3gNu99fJXjn5lh1dhrU8IsVZC8iwnrqW2hbY0TxTGr5NoIyCgDjECuylBVwNHITPI7M7kJt5xqiFbcMQaHiS7wsY7qTeJWV17ZAKMRJQzjgTvhI3UNuWbmZA9zWwZD'
 
@@ -40,6 +30,40 @@ let sendTextMessage = (sender, text)=>{
 	messageData = {
 		text:text
 	}
+}
+
+	let sendGenericMessage=(sender)=> {
+		 let messageData = {
+			 "attachment": {
+				 "type": "template",
+				 "payload": {
+				 "template_type": "generic",
+					 "elements": [{
+					 "title": "First card",
+						 "subtitle": "Element #1 of an hscroll",
+						 "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+						 "buttons": [{
+							 "type": "web_url",
+							 "url": "https://www.messenger.com",
+							 "title": "web url"
+						 }, {
+							 "type": "postback",
+							 "title": "Postback",
+							 "payload": "Payload for first element in a generic bubble",
+						 }],
+					 }, {
+						 "title": "Second card",
+						 "subtitle": "Element #2 of an hscroll",
+						 "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+						 "buttons": [{
+							 "type": "postback",
+							 "title": "Postback",
+							 "payload": "Payload for second element in a generic bubble",
+						 }],
+					 }]
+				 }
+			 }
+		 }
 	request({
 		url:'https:/graph.facebook.com/v2.6/me/messages',
 		qs:{access_token: token},
@@ -55,9 +79,30 @@ let sendTextMessage = (sender, text)=>{
 			}else if (response.body.error){
 				console.log("Error: ", response.body.error)
 			}
-
 	})
 }
+
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'Generic') {
+				sendGenericMessage(sender)
+				continue
+			}
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
+		}
+	}
+	res.sendStatus(200)
+})
 
 app.listen(app.get('port'), ()=>{
 	console.log('running on port', app.get('port'))
