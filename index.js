afb5b8f596c616d1a7ad12ba10ad2cc9c2ef8394
jdrichardstech <jdrichardstech@gmail.com>
2017-06-28 13:45:51 -0400

    try old JS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
 
 app.set('port', (process.env.PORT || 3000))
@@ -10,17 +10,17 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(bodyParser.json)
 
-app.get('/', (req, res)=>{
+app.get('/', function(req, res){
 	res.send('Hello world, I am JD\'s chatbot')
 })
 
-app.get('/webhook/', (req, res)=>{
+app.get('/webhook/', function(req, res){
 	if(req.query['hub.verify_token']==='this_is_my_token'){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
 })
 
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), function(){
 	console.log('running on port', app.get('port'))
 })
