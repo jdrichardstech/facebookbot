2e30c17fa836f2b205a16244cebb4bfee5034906
jdrichardstech <jdrichardstech@gmail.com>
2017-06-28 17:06:03 -0400

    add postback function

--- index.js
+++ index.js
@@ -174,6 +174,23 @@ function receivedMessage(event) {
   }
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
+
 app.listen(app.get('port'), ()=>{
 	console.log('running on port', app.get('port'))
 })
