a4e5c24139042161c48ef6df9014668e4a7a2f28
jdrichardstech <jdrichardstech@gmail.com>
2017-06-28 13:59:05 -0400

    refactor to ES6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
 
 app.set('port', (process.env.PORT || 3000))
@@ -10,17 +10,17 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(bodyParser.json())
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
 	res.send('Hello world, I am JD\'s chatbot')
 })
 
-app.get('/webhook/', function(req, res){
+app.get('/webhook/', (req, res)=>{
 	if(req.query['hub.verify_token']==='this_is_my_token'){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
 })
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), ()=>{
 	console.log('running on port', app.get('port'))
 })
